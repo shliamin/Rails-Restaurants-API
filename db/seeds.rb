134c62a93c9954098e90174ba189b1229e896818
Efim <efimshliamin@gmail.com>
2024-06-26 12:28:59 +0200

    new bundle

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.first || User.create(email: 'example@mail.com', password: '123456')
+
+Restaurant.create(name: "Restaurant One", address: "123 Main Street", user: user)
+Restaurant.create(name: "Restaurant Two", address: "456 Elm Street", user: user)
+Restaurant.create(name: "Restaurant Three", address: "789 Oak Street", user: user)
+
+
